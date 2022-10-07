4113662d1ac97211d54c0359fbc0df581c12100d
kana <lnbtkana2326@gmail.com>
2022-10-07 23:10:09 +0700

    success configuration

--- learn-tailwind/tailwind.config.js
+++ learn-tailwind/tailwind.config.js
@@ -1,8 +1,26 @@
 /** @type {import('tailwindcss').Config} */
+/* extend คือการ config เพิ่มเติมจาก utilities ที่มีอยู่ */
+/* ถ้าหาก congig นอก extend คือการแก้ไข utilities ใหม่ */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    screens:{
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
+    extend: {
+      colors:{
+        'kanatest':'red'
+      },
+      screens:{
+        'small':'300px',
+        'medium':'400px',
+        'large':'500px',
+      },
+    },
   },
   plugins: [],
 }
